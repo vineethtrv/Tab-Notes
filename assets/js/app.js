a709088357310730a0708d65069ecb55ada445aa
Vineeth T R <vineeth.tr@gds.ey.com>
2022-08-20 13:11:57 +0530

    + calculator UI , Calculator move action

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,8 @@
 let newNotes = '';
 let theme = localStorage.getItem('theme');
+const calcEl = document.getElementById("calculator");
+const calcDisplayEl = document.getElementById("calc-display");
+let calcValue = 0;
 var quill = new Quill('#editor', {
       theme: 'snow',
     modules: {
@@ -58,11 +61,67 @@ document.querySelectorAll('.dropdown-menu li').forEach(list => {
 
 
 
+// Drag 
+function dragElement(dragEL) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById("calc-header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById("calc-header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        dragEL.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+
+        let x = (dragEL.offsetLeft - pos1) 
+        let y = (dragEL.offsetTop - pos2); 
+
+        // Prevent move outer window
+        if (x > 0 && y > 0 && 
+            x <= (window.innerWidth - dragEL.clientWidth) &&
+            y <= (window.innerHeight - dragEL.clientHeight) 
+            ){
+            dragEL.style.top = y + "px";
+            dragEL.style.left = x + "px";
+        }
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
 
 
 
 
 
+
+// Toggle calculator
+const toggleCalculator = ()=> {
+    calcEl.classList.toggle('show');
+}
 
 
 // Restore data from previous
@@ -72,10 +131,38 @@ restoreNotes();
 if (theme) {
     document.body.setAttribute('class', theme);
     document.querySelectorAll('.dropdown-menu li').forEach(list => {
-        if (list.dataset.value == theme){
+        if (list.dataset.value == theme) {
             list.classList.add('active');
-        } else{
+        } else {
             list.classList.remove('active');
         }
-    }) 
+    })
 }
+
+// Make calculator movable
+dragElement(calcEl);
+
+
+// Print Numbers
+document.querySelectorAll('.btn-calc').forEach(calcBtn => {
+    // calcDisplayEl
+    calcBtn.addEventListener('click', (e) => {
+        let val = calcBtn.dataset.eval;
+        if(val){
+            calcValue = calcValue === 0? val : calcValue + val;
+
+        }
+
+        // Answer
+        if(calcBtn.getAttribute('id') === 'enter'){
+            calcValue = eval(calcValue);
+        }
+        // Clear calculate value
+        if (calcBtn.getAttribute('id') === 'clear'){
+            calcValue = 0;
+        }
+
+        calcDisplayEl.value = calcValue;
+        console.log(calcValue)
+    })
+})
