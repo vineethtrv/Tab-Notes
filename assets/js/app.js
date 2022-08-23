4cfe952cd0b8542249fa621f63c07ebca5bb9b60
Vineeth T R <vineeth.tr@gds.ey.com>
2022-08-23 19:41:40 +0530

    + calculator tab sync , + theme tab sync

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 let newNotes = '';
-let theme = localStorage.getItem('theme');
+let theme = '';
+let showCalc = localStorage.getItem('showCalc');
 const calcEl = document.getElementById("calculator");
 const calcDisplayEl = document.getElementById("calc-display");
-let calcValue = 0;
+const operators = ['-', '+', '*', '/'];
+let calcValue = '';
 var quill = new Quill('#editor', {
       theme: 'snow',
     modules: {
@@ -31,14 +33,6 @@ quill.on('text-change', function () {
 });
 
 
-// Check tabs are sync with Notes
-window.addEventListener("storage", (event) => {
-    if (event.storageArea != localStorage) return;
-    if (event.key === 'notes') {
-        restoreNotes();
-    }
-})
-
 
 
 // Theme
@@ -57,6 +51,20 @@ document.querySelectorAll('.dropdown-menu li').forEach(list => {
         document.body.setAttribute('class', theme);
     })
 })
+
+restoreTheme = () => {
+    theme = localStorage.getItem('theme');
+    if (theme) {
+        document.body.setAttribute('class', theme);
+        document.querySelectorAll('.dropdown-menu li').forEach(list => {
+            if (list.dataset.value == theme) {
+                list.classList.add('active');
+            } else {
+                list.classList.remove('active');
+            }
+        })
+    }
+}
 
 
 
@@ -97,12 +105,13 @@ function dragElement(dragEL) {
         let y = (dragEL.offsetTop - pos2); 
 
         // Prevent move outer window
-        if (x > 0 && y > 0 && 
-            x <= (window.innerWidth - dragEL.clientWidth) &&
-            y <= (window.innerHeight - dragEL.clientHeight) 
-            ){
-            dragEL.style.top = y + "px";
+        if (x > 0 && x <= (window.innerWidth - dragEL.clientWidth)){
             dragEL.style.left = x + "px";
+            localStorage.setItem("calcX", x + "px")
+        }
+        if ( y > 0 && y <= (window.innerHeight - dragEL.clientHeight)){
+            dragEL.style.top = y + "px";
+            localStorage.setItem("calcY", y + "px")
         }
     }
 
@@ -121,29 +130,16 @@ function dragElement(dragEL) {
 // Toggle calculator
 document.querySelector('.btn-calculator').addEventListener('click', () => {
     calcEl.classList.toggle('show');
+    calcDisplayEl.focus();
+    localStorage.setItem('showCalc', 'true');
 });
 document.querySelector('.btn-close').addEventListener('click', () => {
     calcEl.classList.remove('show');
+    localStorage.setItem('showCalc', 'false');
 });
 
 
-// Restore data from previous
-restoreNotes();
 
-// Theme restore
-if (theme) {
-    document.body.setAttribute('class', theme);
-    document.querySelectorAll('.dropdown-menu li').forEach(list => {
-        if (list.dataset.value == theme) {
-            list.classList.add('active');
-        } else {
-            list.classList.remove('active');
-        }
-    })
-}
-
-// Make calculator movable
-dragElement(calcEl);
 
 
 // Print Numbers
@@ -151,18 +147,14 @@ document.querySelectorAll('.btn-calc').forEach(calcBtn => {
     // calcDisplayEl
     calcBtn.addEventListener('click', (e) => {
         let val = calcBtn.dataset.eval;
-        let operations = ['-','+', '*' , '/'];
+        
         if(val){
-            let lastIndex = calcValue.length - 1;
-            let lastLatter = calcValue[lastIndex];
-            // Remove if lastIndex is operations
-            calcValue = operations.includes(lastLatter) && operations.includes(val) ? calcValue.substring(0, lastIndex) : calcValue; 
             calcValue = calcValue === 0? val : calcValue + val;
-
+            calcValue = operatorsDupeCheck(calcValue);
         }
 
         // Answer
-        if(calcBtn.getAttribute('id') === 'enter'){
+        if (calcBtn.getAttribute('id') === 'enter'){
             calculateValue()
         }
         // Clear calculate value
@@ -171,6 +163,7 @@ document.querySelectorAll('.btn-calc').forEach(calcBtn => {
         }
 
         calcDisplayEl.value = calcValue;
+        localStorage.setItem('calcValue', calcValue);
     });
 })
 
@@ -179,11 +172,15 @@ document.querySelectorAll('.btn-calc').forEach(calcBtn => {
 
 
 const calculateValue = () => {
+    let lastVal = calcValue[calcValue.length - 1];
+    if (calcValue == 0 || operators.includes(lastVal) || lastVal == '.'){
+        return
+    }
     let numbers = calcValue.split(/\+|\-|\*|\//g);
-    let operators = calcValue.replace(/[0-9]|\./g, "").split("");
+    let calcOperators = calcValue.replace(/[0-9]|\./g, "").split("");
     let nextVal = 0;
     let currentVal = 0;
-    operators.forEach((op, i) => {
+    calcOperators.forEach((op, i) => {
         currentVal = i === 0 ? parseFloat(numbers[i]) : currentVal;
         nextVal = parseFloat(numbers[1 + i]);
         switch (op) {
@@ -203,13 +200,14 @@ const calculateValue = () => {
     })
     calcValue = currentVal;
     calcDisplayEl.value = calcValue;
+    localStorage.setItem('calcValue', calcValue);
 }
 
 
 calcDisplayEl.addEventListener('keydown', e => {
-    let allowedKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '*', '/', '.', 'Backspace'];    
+    let allowedKeys = [...operators, '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', 'Backspace'];   
     // Get Answer if pressed Ender or equal
-    if (e.key == "Enter" || e.keyCode == 13 ||e.key == "=" ) {
+    if (e.key == "Enter" || e.keyCode == 13 || e.key == "=") {
         calculateValue();
         e.preventDefault()
         return 
@@ -220,6 +218,103 @@ calcDisplayEl.addEventListener('keydown', e => {
     }
 })
 
-calcDisplayEl.addEventListener('input', e=> {
-   calcValue = calcDisplayEl.value;
+calcDisplayEl.addEventListener('input', e => {
+    calcValue = calcDisplayEl.value = operatorsDupeCheck(calcDisplayEl.value);
+    localStorage.setItem('calcValue', calcValue);
+});
+
+
+
+
+
+
+// Avoid using duplicate operators 
+const operatorsDupeCheck = (str)=>{
+    if (operators.includes(str[0])) {
+        str = str.substring(1, str.length);
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            str = operators.includes(str[i]) && operators.includes(str[i - 1]) ? str.substring(0, i - 1) + str.substring(i, str.length) : str;
+            str = str[i] == '.' && str[i - 1] == '.' ? str.substring(0, i - 1) + str.substring(i, str.length) : str;
+        }
+    }
+    return str;
+}
+
+
+
+
+
+
+
+
+
+
+
+// Restore data from previous
+restoreNotes();
+
+// Theme restore
+restoreTheme()
+
+// Make calculator movable
+dragElement(calcEl);
+
+// Open Calculator
+if (showCalc === 'true') {
+    // Calculator position
+    calcEl.style.left = localStorage.getItem("calcX") ? localStorage.getItem("calcX") : "0px";
+    calcEl.style.top = localStorage.getItem("calcY") ? localStorage.getItem("calcY") : "0px";
+    calcEl.classList.add('show');
+    calcDisplayEl.value = calcValue = localStorage.getItem("calcValue");
+    calcDisplayEl.focus();
+}
+
+
+// Window resize
+window.addEventListener('resize', (e) => {
+    let vw = window.innerWidth;
+    let vh = window.innerHeight;
+
+    if (calcEl.offsetLeft > (vw - calcEl.clientWidth)) {
+        let x = (vw - calcEl.clientWidth);
+        calcEl.style.left = x > 0 ? x + 'px' : '0px';
+        localStorage.setItem("calcX", calcEl.style.left);
+    }
+    if (calcEl.offsetTop > (vh - calcEl.clientHeight)) {
+        let y = (vh - calcEl.clientHeight);
+        calcEl.style.top = y > 0 ? y + 'px' : '0px';
+        localStorage.setItem("calcY", calcEl.style.top);
+    }
+})
+
+
+
+
+// Check tabs are sync with Notes
+window.addEventListener("storage", (event) => {
+    if (event.storageArea != localStorage) return;
+    if (event.key === 'notes') {
+        restoreNotes();
+    }
+    if (event.key === 'theme') {
+        restoreTheme();
+    }
+    
+    if (event.key === 'showCalc') {
+        if(localStorage.getItem('showCalc') === true){
+            calcEl.classList.add('show');
+        } else{
+            calcEl.classList.remove('show');
+        }
+    }
+    if (event.key === 'calcValue') {
+        calcDisplayEl.value = calcValue = localStorage.getItem("calcValue");
+    }
+    if (event.key === 'calcX') {
+        calcEl.style.left = localStorage.getItem("calcX") ? localStorage.getItem("calcX") : "0px";
+    }
+    if (event.key === 'calcY') {
+        calcEl.style.top = localStorage.getItem("calcY") ? localStorage.getItem("calcY") : "0px";
+    }
 })
